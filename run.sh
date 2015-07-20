283b94c44888b246c710e313e247e07bd5a40147
jaxgeller <jacksongeller@gmail.com>
2015-07-20 09:30:05 -0400

    add option to mount volume of local theme

--- run.sh
+++ run.sh
@@ -9,6 +9,9 @@ read USER
 echo -n "wp-admin password: "
 read -s PASSWD
 
+echo -n "local wp theme (will mount into docker): "
+read -s THEME
+
 # Get initial login cookie of the current install
 LOGIN_COOKIE=$( \
   curl -s \
@@ -40,6 +43,7 @@ docker run \
   --name wp \
   --link mysql:mysql \
   -p 8000:80 \
+  -v $(pwd)/$THEME:/var/www/html/wp-content/themes/$THEME \
   -d wordpress 2>&1 >/dev/null
 
 # get docker ip address
@@ -136,6 +140,13 @@ if [[ $? -ne 0 ]]; then
   exit 1
 fi
 
-# Clean, then all done
+# clean
 rm backup.wp.xml
+
+# enable wp debug
+docker exec \
+  -d wp \
+  sed -i "s/'WP_DEBUG', false/'WP_DEBUG', true/g" wp-config.php \
+  2>&1 >/dev/null
+
 echo "all done!"
